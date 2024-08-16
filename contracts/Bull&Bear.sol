1232287baad248bb21398c176be8a7bf453d4b34
Matt Rogers <rogers.matthew35@gmail.com>
2024-08-16 12:41:21 -0500

    contracts new 17

--- contracts/Bull&Bear.sol
+++ contracts/Bull&Bear.sol
@@ -15,7 +15,7 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 // ./interfaces/KeeperCompatibleInterface.sol ..
 import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
 
-// Dev imports.This only works  on a  local dev network JP JP 
+// Dev imports.This only works  on a  local dev  network JP JP 
 // and will not work  on any test or main livenets. 
 import "hardhat/console.sol";
 
